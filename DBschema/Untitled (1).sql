475d4e0ff9f5d13b005d397881bf0d206400aa38
taofeeq <hamzataofeeq@gmail.com>
2019-11-26 19:27:56 -0800

    Update Untitled (1).sql

--- DBschema/Untitled (1).sql
+++ DBschema/Untitled (1).sql
@@ -1,43 +1,43 @@
-CREATE TABLE `employees` (
-  `emp_no` INT(11),
-  `birth_date` date,
-  `first_name` varchar(20),
-  `last_name` varchar(20),
-  `email` varchar(50),
-  `phone_number` varchar(15)
+CREATE TABLE IF NOT EXISTS employees (
+  emp_no INT(11) NOT NULL,
+  birth_date date NOT NULL,
+  first_name varchar(20) NOT NULL,
+  last_name varchar(20) NOT NULL,
+  email varchar(50) NOT NULL,
+  phone_number varchar(15) NOT NULL
 );
 
-CREATE TABLE `salaries` (
-  `emp_no` INT(11),
-  `Salary` INT(11),
-  `from_date` date,
-  `to_date` date
+CREATE TABLE IF NOT EXISTS salaries (
+  emp_no INT(11) NOT NULL,
+  Salary INT(11) NOT NULL,
+  from_date date NOT NULL,
+  to_date date NOT NULL
 );
 
-CREATE TABLE `titles` (
-  `emp_no` INT(11),
-  `title` varchar(50),
-  `from_date` date,
-  `to_date` date
+CREATE TABLE IF NOT EXISTS titles (
+  emp_no INT(11) NOT NULL,
+  title varchar(50) NOT NULL,
+  from_date date NOT NULL,
+  to_date date NOT NULL
 );
 
-CREATE TABLE `dept_manager` (
-  `dept_no` CHAR(4),
-  `emp_no` INT(11),
-  `from_date` date,
-  `to_date` date
+CREATE TABLE IF NOT EXISTS dept_manager (
+  dept_no CHAR(4) NOT NULL,
+  emp_no INT(11) NOT NULL,
+  from_date date NOT NULL,
+  to_date date NOT NULL
 );
 
-CREATE TABLE `departments` (
-  `dept_no` char(4),
-  `dept_name` varchar(40)
+CREATE TABLE IF NOT EXISTS departments (
+  dept_no char(4) NOT NULL,
+  dept_name varchar(40) NOT NULL
 );
 
-CREATE TABLE `emp_Dept` (
-  `emp_no` INT(11),
-  `dept_no` CHAR(4),
-  `from_date` date,
-  `to_date` date
+CREATE TABLE IF NOT EXISTS emp_Dept (
+  emp_no INT(11) NOT NULL,
+  dept_no CHAR(4) NOT NULL,
+  from_date date NOT NULL,
+  to_date date NOT NULL
 );
 
 ALTER TABLE `emp_Dept` ADD FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`);
